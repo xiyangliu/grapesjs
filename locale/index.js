f48ec80e73b9328422ed8fef0c6350e870a7179d
Xiyang Liu <yippeecai@gmail.com>
2019-11-26 19:38:53 -0800

    no message

---
+++ locale/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "en", {
+  enumerable: true,
+  get: function get() {
+    return _en.default;
+  }
+});
+Object.defineProperty(exports, "it", {
+  enumerable: true,
+  get: function get() {
+    return _it.default;
+  }
+});
+Object.defineProperty(exports, "tr", {
+  enumerable: true,
+  get: function get() {
+    return _tr.default;
+  }
+});
+
+var _en = _interopRequireDefault(require("./en"));
+
+var _it = _interopRequireDefault(require("./it"));
+
+var _tr = _interopRequireDefault(require("./tr"));
