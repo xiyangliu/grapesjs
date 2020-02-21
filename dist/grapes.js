ab93f674f4271e3a4fd8042ac659d6609cfa8c77
Xiyang Liu <yippeecai@gmail.com>
2020-02-20 22:30:36 -0800

    Updated color

--- dist/grapes.js
+++ dist/grapes.js
@@ -36856,7 +36856,7 @@ var defaultConfig = {
   editors: editors,
   plugins: plugins,
   // Will be replaced on build
-  version: '0.15.12',
+  version: '0.15.13',
 
   /**
    * Initialize the editor with passed options
